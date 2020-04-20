a0a0bd3f393c22300c64234e1878a00a1d5ec87a
anhduc1150 <61228500+anhduc1150@users.noreply.github.com>
2020-04-20 21:02:28 +0700

    Add authenticate req
    Add auth requirement to access chatroom

--- app/Http/Controllers/ChatroomController.php
+++ app/Http/Controllers/ChatroomController.php
@@ -10,6 +10,10 @@ use Illuminate\Support\Facades\Auth;
 
 class ChatroomController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');    
+    }
     public function send(Request $request){
 
         
