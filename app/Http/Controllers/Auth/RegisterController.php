9cd313f0a4c57ebf7c36022d12872b50d1202f6e
hoan902 <62045896+hoan902@users.noreply.github.com>
2020-04-24 12:48:50 +0700

    Allow only staff to register user (fix in Register middleware), prevent auto-login when create new account

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,8 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
 
 class RegisterController extends Controller
 {
@@ -23,13 +27,43 @@ class RegisterController extends Controller
     */
 
     use RegistersUsers;
+    //Handle a registration request for the application.
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        event(new Registered($user = $this->create($request->all())));
+
+
+        if ($response = $this->registered($request, $user)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new Response('', 201)
+            : redirect($this->redirectPath());
+    }
 
     /**
      * Where to redirect users after registration.
-     *
-     * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    //protected $redirectTo = RouteServiceProvider::HOME;
+
+    //Redirect to each create form depend on user_type to continue insert profile info of user
+    protected function redirectTo()
+    {
+        if (User::all()->last()->user_type == 'student') {
+            return '/students/create';
+        }
+        else if (User::all()->last()->user_type == 'tutor') {
+            return '/tutors/create';
+        }
+        else if (User::all()->last()->user_type == 'staff') {
+            return '/staffs/create';
+        }
+        return '/home';
+    }
+
 
     /**
      * Create a new controller instance.
@@ -38,7 +72,9 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        //middleware stop process if user is a guest
+        $this->middleware('auth')->except('logout');
+        //middleware stop user if they are not staff (didn't add the error exception yet)
         $this->middleware('staff');
     }
 
@@ -70,5 +106,6 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
             'user_type'=> $data['user_type'],
         ]);
+        //should have a successful flash message to show. But idk how to do it yet
     }
 }
