49e244ac5fc602c3c95eb61290e7557900f53634
Krönus <slowemaste@gmail.com>
2020-04-28 23:02:33 +0700

    Remake interfaces, added fonts, css, javascript, and images

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,43 @@
+<header>
+    <!-- Header Start -->
+    <div class="header-area header-transparrent ">
+        <div class="main-header header-sticky">
+            <div class="container">
+                <div class="row align-items-center">
+                    <!-- Logo -->
+                    <div class="col-xl-2 col-lg-2 col-md-2">
+                        <div class="logo">
+                            <a href="/"><img src="img/logo/interstudy.png" alt="" width="200px" height="50px"></a>
+                        </div>
+                    </div>
+                    <div class="col-xl-10 col-lg-10 col-md-10">
+                        <!-- Main-menu -->
+                        <div class="main-menu f-right d-none d-lg-block">
+                            <nav>
+                                <ul id="navigation">
+                                    <li class="active"><a href="/"> Home</a></li>
+                                    <li><a href="feature.html">Feature</a></li>
+                                    <li><a href="services.html">Services</a></li>
+                                    <li><a href="pricing.html">Pricing</a></li>
+                                    <li><a href="#">Pages</a>
+                                        <ul class="submenu">
+                                            <li><a href="blog.html">Blog</a></li>
+                                            <li><a href="single-blog.html">Blog Details</a></li>
+                                            <li><a href="elements.html">Element</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="contact.html">Contact</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <!-- Mobile Menu -->
+                    <div class="col-12">
+                        <div class="mobile_menu d-block d-lg-none"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Header End -->
+</header>
