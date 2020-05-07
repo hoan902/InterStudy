f07bee7f8694b91d8826372ceaebe211c0c92421
Krönus <slowemaste@gmail.com>
2020-05-07 17:17:01 +0700

    Fixed staff view

--- resources/views/staffs/index.blade.php
+++ resources/views/staffs/index.blade.php
@@ -2,26 +2,21 @@
 @extends('layouts.preloader')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header"><h3>Staffs list</h3></div>
-
-                    <div class="card-body">
-                            <ul>
-                                @forelse($Staff as $staffs)
-                                    <li>
-                                        <strong>
-                                            <a href="/staffs/{{$staffs->id}}">{{ $staffs->name }}</a>
-                                        </strong>({{ $staffs->phone }})
-                                    </li>
-                                @empty
-                                    <p>No staff to show</p>
-                                @endforelse
-                            </ul>
-                    </div>
-                </div>
+    <div class="h-50">
+        <div class="card">
+            <div class="card-header"><h3>Staffs list</h3></div>
+            <div class="card-body col-auto table-responsive text-nowrap">
+                <ul>
+                    @forelse($Staff as $staffs)
+                        <li>
+                            <strong>
+                                <a class="btn-link" href="/staffs/{{$staffs->id}}">{{ $staffs->name }}</a>
+                            </strong>({{ $staffs->phone }})
+                        </li>
+                    @empty
+                        <p>No staff to show</p>
+                    @endforelse
+                </ul>
             </div>
         </div>
     </div>
